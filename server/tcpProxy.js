acbfd6ea5bdd062ac45d60806269f206efca6fe5
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-12-17 23:30:46 +0800

    try fix mod domain header problem

--- server/tcpProxy.js
+++ server/tcpProxy.js
@@ -89,7 +89,7 @@ function getMinecraftDomain(data) {
         let domain = data.toString('utf8', offset, offset + addressLength);
         domain = domain.replaceAll(" ", "");
         console.log('Domain:', domain);
-        console.log('Domain:', domain.endsWith('FML3'));
+        console.log(`Domain:'${domain.endsWith('FML3')}'`);
         if (domain.endsWith('FML3')) {
             return domain.slice(0, -4);
         } 
